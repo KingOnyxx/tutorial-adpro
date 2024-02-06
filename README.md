53aa7be54bfaf72b6b73c62374cb742329a72d14
Clarence Grady <clarence.grady@ui.ac.id>
2024-02-07 01:32:50 +0700

    update CreateProductFunctionalTest.java and Readme

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Clean code principles and secure coding practices that have been applied to my c
 - Functions that have no side effects.
 - Functions do not return null.
 - I don't use comments because the code is self-explanatory.
+
+Because there is no authorization or authentication, anyone can brute force the URL link and change the list. To improve the source code, i would add some security so that other random people dont easily change the list
+
+Reflection 2
+
+1. After making the unit test, i feel confident in my code's functionality. The number of unit tests in a class can vary depending on the complexity of the class. It is best to have enough tests to cover different scenarios and edge cases. While having 100% code coverage is good, it does not guarantee that there are no bugs or errors in the code. It only indicates that all lines of code have been executed at least once during testing without errors or bugs.
+
+2. I dont think the code is considered clean and could possibly reduce the code quality. If it were to happen, there could be code duplication, Low cohesion between each class, and high coupling. Code duplication can lead to harder maintenance. Low cohesion is If a class is doing too much or not well-defined, it can become harder to understand and difficult to maintain. High Coupling is when you change something in one class and the other class needs to change because of you changed something in the first class. High coupling can make the code more fragile and harder to be developed. I would suggest using encapsulation, single responsibility principle, descriptive names, code formatting, and refactoring. These practices can help to make code cleaner, readable and easy to maintain.
--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -3,25 +3,27 @@ package id.ac.ui.cs.advprog.eshop.functional;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import ch.qos.logback.core.util.Duration;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import java.util.List;
+
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ExtendWith(SeleniumJupiter.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CreateProductFunctionalTest {
     /**
      * The port number assigned to the running application during test execution.
@@ -44,38 +46,58 @@ class CreateProductFunctionalTest {
     }
 
     @Test
+    @Order(1)
     void create_isCorrect(ChromeDriver driver) throws Exception {
         driver.get(String.format("%s:%d/%s/%s", testBaseUrl, serverPort, "product", "create"));
         driver.findElement(By.id("nameInput")).sendKeys("Product 1");
         driver.findElement(By.id("quantityInput")).sendKeys("10000");
         driver.findElement(By.tagName("button")).click();
+        driver.get(baseUrl);
         WebElement element = driver.findElement(By.xpath("//*[text()='Product 1']"));
         assertEquals("Product 1",element.getText());
     }
 
     @Test
+    @Order(2)
     void edit_isCorrect(ChromeDriver driver) throws Exception {
-        create_isCorrect(driver);
-        WebElement productElement = driver.findElement(By.xpath("//*[text()='Product 1']"));
+        driver.get(baseUrl);
+        driver.get(String.format("%s:%d/%s/%s", testBaseUrl, serverPort, "product", "create"));
+        driver.findElement(By.id("nameInput")).sendKeys("Product 2");
+        driver.findElement(By.id("quantityInput")).sendKeys("20000");
+        driver.findElement(By.tagName("button")).click();
+        driver.get(baseUrl);
+        WebElement productElement = driver.findElement(By.xpath("//*[text()='Product 2']"));
         String id = productElement.getAttribute("id");
         driver.get(String.format("%s:%d/%s/%s/%s", testBaseUrl, serverPort, "product", "edit", id));
         WebElement nameInput = driver.findElement(By.id("nameInput"));
         nameInput.clear();
-        nameInput.sendKeys("Product 2");
+        nameInput.sendKeys("Product 3");
         WebElement quantityInput = driver.findElement(By.id("quantityInput"));
         quantityInput.clear();
         quantityInput.sendKeys("100");
         driver.findElement(By.tagName("button")).click();
         WebElement newElement = driver.findElement(By.id(id));
-        assertEquals("Product 2", newElement.getText());
+        assertEquals("Product 3", newElement.getText());
     }
 
     @Test
+    @Order(3)
     void delete_isCorrect(ChromeDriver driver) throws Exception {
-        create_isCorrect(driver);
+        driver.get(baseUrl);
         WebElement productElement = driver.findElement(By.xpath("//*[text()='Product 1']"));
         String id = productElement.getAttribute("id");
         WebElement deleteButton = driver.findElement(By.id(String.format("%s-%s", "delete", id )));
         deleteButton.click();
+        
+        // Switch to the alert window
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        assertEquals("Are you sure you want to delete this product?", alertText);
+        alert.accept();
+
+        driver.get(baseUrl);
+        List<WebElement> rows = driver.findElements(By.tagName("tr"));
+        int rowCount = rows.size();
+        assertEquals(2, rowCount); //header and Product 3
     }
 }
